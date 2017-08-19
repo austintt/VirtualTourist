d25463627bb5debb9d7b633daa6711b04a39b957
Austin Tooley <austin@tooley.me>
2017-08-19 16:38:52 -0500

    successfully calling flickr api

--- VirtualTourist/AlbumViewController.swift
+++ VirtualTourist/AlbumViewController.swift
@@ -12,12 +12,17 @@ import MapKit
 class AlbumViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var infoLabel: UILabel!
     
     var location: Location?
     let radius = 200
+    let flickr = FlickrManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureInfoLabel()
+        
+       
         
     }
     
@@ -30,15 +35,33 @@ class AlbumViewController: UIViewController {
             
             // Configure map
             setUpMap(location: location)
+            
+            // flickr
+            flickr.searchByLocation(location: location) { (result, error) in
+                if let error = error {
+                    self.infoLabel.text = error.localizedDescription
+                } else {
+                    print("yay")
+                }
+            }
+                
         }
     }
     
+    func configureInfoLabel() {
+        infoLabel.layer.borderColor = UIColor(red: 33/255, green: 160/255, blue: 160/255, alpha: 1).cgColor
+        infoLabel.layer.borderWidth = 2.0
+        infoLabel.layer.cornerRadius = 6
+        infoLabel.layer.masksToBounds = true
+        infoLabel.text = ""
+        infoLabel.isHidden = true
+    }
     
-    
+    // MARK: Map
     func setUpMap(location: Location) {
         
         let regionRadius: CLLocationDistance = 2000
-//        performUIUpdatesOnMain {
+        
             // Center map on location
             self.mapView.centerMapOnLocation(location: location, radius: regionRadius)
         
@@ -46,7 +69,10 @@ class AlbumViewController: UIViewController {
             let annotation = MKPointAnnotation()
             annotation.coordinate = CLLocationCoordinate2D(latitude: location.lat, longitude: location.long)
             self.mapView.addAnnotation(annotation)
-//        }
     }
+    
+    // MARK: Collection View
+    
+    // MARK: Network
     
 }
---
+++ VirtualTourist/FlickrManager.swift
@@ -0,0 +1,140 @@
+//
+//  FlickrManager.swift
+//  VirtualTourist
+//
+//  Created by Austin Tooley on 8/19/17.
+//  Copyright © 2017 Austin Tooley. All rights reserved.
+//
+
+import Foundation
+
+class FlickrManager: NSObject {
+    
+    // MARK: Initializer
+    var session = URLSession.shared
+
+    // MARK: API
+    func searchByLocation(location: Location, completionHandlerForSearchByLocation: @escaping (_ result: [String:AnyObject]?, _ error: NSError?) -> Void) {
+        
+        let methodParameters = [
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.SearchMethod,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.BoundingBox: bboxString(location: location),
+            Constants.FlickrParameterKeys.SafeSearch: Constants.FlickrParameterValues.UseSafeSearch,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+        ]
+        
+        getPhotos(methodParameters as [String : AnyObject]) { (results, error) in
+            if let error = error {
+                completionHandlerForSearchByLocation(nil, error)
+            } else {
+                completionHandlerForSearchByLocation(results, nil)
+            }
+        }
+    }
+    
+    private func bboxString(location: Location?) -> String {
+        // ensure bbox is bounded by minimum and maximums
+        if let location = location {
+            let latitude = location.lat
+            let longitude = location.long
+            let minimumLon = max(longitude - Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.0)
+            let minimumLat = max(latitude - Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.0)
+            let maximumLon = min(longitude + Constants.Flickr.SearchBBoxHalfWidth, Constants.Flickr.SearchLonRange.1)
+            let maximumLat = min(latitude + Constants.Flickr.SearchBBoxHalfHeight, Constants.Flickr.SearchLatRange.1)
+            return "\(minimumLon),\(minimumLat),\(maximumLon),\(maximumLat)"
+        } else {
+            return "0,0,0,0"
+        }
+    }
+    
+    private func getPhotos(_ methodParameters: [String: AnyObject], completionHandlerForGetPhotos: @escaping (_ result: [String:AnyObject]?, _ error: NSError?) -> Void) {
+        // create session and request
+        let request = URLRequest(url: flickrURLFromParameters(methodParameters))
+        
+        // create network request
+        let task = session.dataTask(with: request) { (data, response, error) in
+            
+            // if an error occurs, print it and re-enable the UI
+            func sendError(_ error: String) {
+                print(error)
+                let userInfo = [NSLocalizedDescriptionKey : error]
+                completionHandlerForGetPhotos(nil, NSError(domain: "getPhotos", code: 1, userInfo: userInfo))
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            // parse the data
+            let parsedResult: [String:AnyObject]!
+            do {
+                parsedResult = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:AnyObject]
+            } catch {
+                sendError("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult[Constants.FlickrResponseKeys.Status] as? String, stat == Constants.FlickrResponseValues.OKStatus else {
+                sendError("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is "photos" key in our result? */
+            guard let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String:AnyObject] else {
+                sendError("Cannot find keys '\(Constants.FlickrResponseKeys.Photos)' in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is "pages" key in the photosDictionary? */
+            guard let totalPages = photosDictionary[Constants.FlickrResponseKeys.Pages] as? Int else {
+                sendError("Cannot find key '\(Constants.FlickrResponseKeys.Pages)' in \(photosDictionary)")
+                return
+            }
+            
+            // pick a random page!
+            let pageLimit = min(totalPages, 40)
+            let randomPage = Int(arc4random_uniform(UInt32(pageLimit))) + 1
+            print(photosDictionary)
+            completionHandlerForGetPhotos(photosDictionary, nil)
+//            self.displayImageFromFlickrBySearch(methodParameters, withPageNumber: randomPage)
+        }
+        
+        // start the task!
+        task.resume()
+    }
+    
+    private func flickrURLFromParameters(_ parameters: [String:AnyObject]) -> URL {
+        
+        var components = URLComponents()
+        components.scheme = Constants.Flickr.APIScheme
+        components.host = Constants.Flickr.APIHost
+        components.path = Constants.Flickr.APIPath
+        components.queryItems = [URLQueryItem]()
+        
+        for (key, value) in parameters {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            components.queryItems!.append(queryItem)
+        }
+        
+        return components.url!
+    }
+    
+}
